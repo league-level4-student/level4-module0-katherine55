a92fba73658d6a9992356102c81057549601d7dc
League <league@WTS9.attlocal.net>
2018-09-30 11:23:48 -0700

    saved
    new filews

--- src/_04_Maze_Maker/Maze.java
+++ src/_04_Maze_Maker/Maze.java
@@ -1,31 +1,39 @@
 package _04_Maze_Maker;
+
 import java.awt.Graphics;
 
 public class Maze {
-	//1. Create a 2D array of cells. Don't initialize it.
-
+	// 1. Create a 2D array of cells. Don't initialize it.
+	Cell[][] boxes;
 	private int width;
 	private int height;
 
 	public Maze(int w, int h) {
 		this.width = w;
 		this.height = h;
-
-		//2. Initialize the cells using the width and height varibles
-
-		//3. Iterated through each cell and initialize it
-		//   using i and j as the location
-		
+		boxes = new Cell[w][h];
+		// 2. Initialize the cells using the width and height variables
+		for (int i = 0; i < boxes.length; i++) {
+			for (int j = 0; j < boxes[i].length; j++) {
+				boxes[i][j] = new Cell(i, j);
+			}
+		}
+		// 3. Iterated through each cell and initialize it
+		// using i and j as the location
 	}
 
-	//4. This method iterates through the cells and draws them
+	// 4. This method iterates through the cells and draws them
 	public void draw(Graphics g) {
-		
+		for (int i = 0; i < boxes.length; i++) {
+			for (int j = 0; j < boxes[i].length; j++) {
+				boxes[i][j].draw(g);
+			}
+		}
 	}
-	
-	//4b. This method returns the selected cell.
-	public Cell getCell(int x, int y){
-		return  null;
+
+	// 4b. This method returns the selected cell.
+	public Cell getCell(int x, int y) {
+		return boxes[x][y];
 	}
 
 	public int getWidth() {
--- src/_04_Maze_Maker/MazeMaker.java
+++ src/_04_Maze_Maker/MazeMaker.java
@@ -4,72 +4,75 @@ import java.util.ArrayList;
 import java.util.Random;
 import java.util.Stack;
 
+public class MazeMaker {
 
-public class MazeMaker{
-	
 	private static int width;
 	private static int height;
-	
+
 	private static Maze maze;
-	
+
 	private static Random randGen = new Random();
 	private static Stack<Cell> uncheckedCells = new Stack<Cell>();
-	
-	
-	public static Maze generateMaze(int w, int h){
+
+	public static Maze generateMaze(int w, int h) {
 		width = w;
 		height = h;
 		maze = new Maze(width, height);
-		
-		//4. select a random cell to start
-		
-		
-		//5. call selectNextPath method with the randomly selected cell
-		
-		
+		// 4. select a random cell to start
+		selectNextPath(maze.getCell(randGen.nextInt(w), randGen.nextInt(h)));
+		// 5. call selectNextPath method with the randomly selected cell
 		return maze;
 	}
 
-	//6. Complete the selectNextPathMethod
+	// 6. Complete the selectNextPathMethod
 	private static void selectNextPath(Cell currentCell) {
-		//A. mark cell as visited
+		// A. mark cell as visited
+		currentCell.setBeenVisited(true);
 
-		//B. check for unvisited neighbors using the cell
-		
-		//C. if has unvisited neighbors,
-		
-			//C1. select one at random.
-			
-			//C2. push it to the stack
-		
-			//C3. remove the wall between the two cells
+		// B. check for unvisited neighbors using the cell
 
-			//C4. make the new cell the current cell and mark it as visited
-			
-			
-		//D. if all neighbors are visited
-		
-			//D1. if the stack is not empty
-			
-				// D1a. pop a cell from the stack
-		
-				// D1b. make that the current cell
-				
-			
-		
+		// C. if has unvisited neighbors,
+
+		// C1. select one at random.
+
+		// C2. push it to the stack
+
+		// C3. remove the wall between the two cells
+
+		// C4. make the new cell the current cell and mark it as visited
 	}
+	// D. if all neighbors are visited
 
-	//7. Complete the remove walls method.
-	//   This method will check if c1 and c2 are adjacent.
-	//   If they are, the walls between them are removed.
+	// D1. if the stack is not empty
+
+	// D1a. pop a cell from the stack
+
+	// D1b. make that the current cell
+
+	// 7. Complete the remove walls method.
+	// This method will check if c1 and c2 are adjacent.
+	// If they are, the walls between them are removed.
 	private static void removeWalls(Cell c1, Cell c2) {
-		
+
 	}
-	
-	//8. Complete the getUnvisitedNeighbors method
-	//   Any unvisited neighbor of the passed in cell gets added
-	//   to the ArrayList
+
+	// 8. Complete the getUnvisitedNeighbors method
+	// Any unvisited neighbor of the passed in cell gets added
+	// to the ArrayList
 	private static ArrayList<Cell> getUnvisitedNeighbors(Cell c) {
-		return null;
+		ArrayList<Cell> poss = new ArrayList<Cell>();
+		if (!maze.getCell(c.getX() - 1, c.getY()).hasBeenVisited()) {
+			poss.add(maze.getCell(c.getX() - 1, c.getY()));
+		}
+		if (!maze.getCell(c.getX(), c.getY() + 1).hasBeenVisited()) {
+			maze.getCell(c.getX(), c.getY() + 1);
+		}
+		if (!maze.getCell(c.getX() + 1, c.getY()).hasBeenVisited()) {
+			maze.getCell(c.getX() + 1, c.getY());
+		}
+		if (!maze.getCell(c.getX(), c.getY() - 1).hasBeenVisited()) {
+			maze.getCell(c.getX(), c.getY() - 1);
+		}
+		return poss;
 	}
 }
